ddac5dfdcf36729bdde02b0386a366bb4a4d6763
Eda Akturk <edaakturk@Edas-MacBook-Pro.local>
2024-03-04 13:17:10 -0500

    add frog

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,67 @@
-export const runtime = 'edge'
+import { Button, Frog, TextInput } from 'frog'
+import { handle } from 'frog/vercel'
 
-const image = 'https://www.prosportstickers.com/wp-content/uploads/nc/u/spongebob_wallpaper_sticker_9__56561.jpg'
+// Uncomment to use Edge Runtime.
+export const config = {
+   runtime: 'edge',
+ }
 
-export default function Home() {
-  const meta = {
-    'og:image': image,
-    'fc:frame': 'vNext',
+export const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub API URL to enable frame verification.
+  // hubApiUrl: 'https://api.hub.wevm.dev',
+})
 
-    'fc:frame:image': image,
-  
-    'fc:frame:button:1': "button 1",
-    'fc:frame:button:1:action': 'link',
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {status === 'response'
+            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
+            : 'Welcome!'}
+        </div>
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Enter custom fruit..." />,
+      <Button value="apples">Apples</Button>,
+      <Button value="oranges">Oranges</Button>,
+      <Button value="bananas">Bananas</Button>,
+      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
 
-    'fc:frame:button:2': "button 2",
-    'fc:frame:button:2:action': 'link',
-
-    'fc:frame:input:text': "what's your idea?",
-  }
-
-  return {
-    other: {
-      ...meta
-    },
-  }
-}
-
-
+export const GET = handle(app)
+export const POST = handle(app)
