c2075dc7cb47a932c6f6824a39e7c8860dfe7a2c
Eda Akturk <edaakturk@Edas-MacBook-Pro.local>
2024-03-04 10:49:49 -0500

    fix frame

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export const runtime = 'edge'
 
-const image = 'https://i.imgur.com/jkNjTAe.gif'
+const image = 'https://www.prosportstickers.com/wp-content/uploads/nc/u/spongebob_wallpaper_sticker_9__56561.jpg'
 
 export default function Home() {
   const meta = {
@@ -8,32 +8,17 @@ export default function Home() {
     'fc:frame': 'vNext',
 
     'fc:frame:image': image,
-    'fc:frame:image:aspect_ratio': '1:1',
   
-    'fc:frame:button:1': "create from your idea",
-    'fc:frame:button:1:action': 'post',
+    'fc:frame:button:1': "button 1",
+    'fc:frame:button:1:action': 'link',
 
-    'fc:frame:button:2': "image from random idea",
-    'fc:frame:button:2:action': 'post',
+    'fc:frame:button:2': "button 2",
+    'fc:frame:button:2:action': 'link',
 
     'fc:frame:input:text': "what's your idea?",
-
-    'hey:portal': 'vLatest',
-    'hey:portal:image': image,
-    'hey:portal:button:1': 'Generate my image',
-    'hey:portal:button:1:type': 'submit'
   }
 
   return {
-    openGraph: {
-      images: [
-        {
-          url: image,
-          width: '1000',
-          height: '1000'
-        }
-      ]
-    },
     other: {
       ...meta
     },
